8f9945e0a137ad374c84283070b22fc0843b0276
Nathan Owsiany <nowsiany@gmail.com>
2015-04-09 15:56:00 -0600

    configure initial dataset routing

--- app/routes/dataset.js
+++ app/routes/dataset.js
@@ -1,4 +1,14 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  dataset: null,
+
+  model: function(params) {
+    this.set('dataset', params.dataset);
+    return params.dataset;
+  },
+
+  setupController: function(controller, model) {
+    controller.set('dataset', this.get('dataset'));
+  }
 });
