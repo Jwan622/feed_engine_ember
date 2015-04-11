872c0038b6d0400aa92c6cd61ed825c74400a4b5
Nathan Owsiany <nowsiany@gmail.com>
2015-04-10 20:32:13 -0600

    added model RSVP to dataset route

--- app/controllers/dataset.js
+++ app/controllers/dataset.js
@@ -1,4 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  tweets: Ember.computed.alias('model.tweets.tweets'),
+  sentimentScore: Ember.computed.alias('model.tweets.sentiment.average'),
+
+  sentimentScoreDisplay: function() {
+    var score = this.get('sentimentScore');
+    return parseFloat(score, 10).toFixed(2);
+  }.property('sentimentScore')
 });
