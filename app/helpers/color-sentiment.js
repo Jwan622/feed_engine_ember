51c203d8e665445f43ed61f4950d10c5bf70a438
Nathan Owsiany <nowsiany@gmail.com>
2015-04-12 13:48:49 -0600

    added format-date helper to format tweet dates

--- app/helpers/color-sentiment.js
+++ app/helpers/color-sentiment.js
@@ -1,6 +1,6 @@
 import Ember from 'ember';
 
-export default Ember.Handlebars.makeBoundHelper(function(value, options) {
+export default Ember.Handlebars.makeBoundHelper(function(value) {
   if (value > 0) {
     return new Ember.Handlebars.SafeString('pos-sentiment');
   } else if (value < 0) {
