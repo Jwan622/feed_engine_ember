1ab89578d438955340f93d10c8757a3aa68b261a
Nathan Owsiany <nowsiany@gmail.com>
2015-04-12 13:26:55 -0600

    setup map-view didInsertElement event to render basic d3 map

--- app/controllers/dataset.js
+++ app/controllers/dataset.js
@@ -2,9 +2,10 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
   tweets: Ember.computed.alias('model.tweets.tweets'),
+  data: Ember.computed.alias('model.data'),
   sentimentScore: Ember.computed.alias('model.tweets.sentiment.average'),
 
   limitedTweets: function() {
     return this.get('tweets').slice(0,3);
-  }.property('tweets')
+  }.property('tweets'),
 });
