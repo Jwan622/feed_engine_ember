54bd497c28a931e230d8d630aee7e7507f57505b
Nathan Owsiany <nowsiany@gmail.com>
2015-04-11 20:47:38 -0600

    updated site styling, bring the darkness

--- Brocfile.js
+++ Brocfile.js
@@ -9,6 +9,7 @@ var app = new EmberApp();
 
 app.import('bower_components/bootstrap/dist/js/bootstrap.js');
 app.import('bower_components/bootstrap/dist/css/bootstrap.css');
+app.import('bower_components/d3/d3.min.js');
 
 // If you need to use different assets in different
 // environments, specify an object as the first parameter. That
