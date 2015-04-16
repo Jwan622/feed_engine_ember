075ab9bb6ff8f58352d2891bab94de4a9781873b
Nathan Owsiany <nowsiany@gmail.com>
2015-04-16 16:22:52 -0600

    Add URL prefix

--- app/routes/dataset.js
+++ app/routes/dataset.js
@@ -1,11 +1,14 @@
 import Ember from 'ember';
+import ENV from 'usausa-ember/config/environment'
+
+const url = ENV.apiUrl;
 
 export default Ember.Route.extend({
   model: function(params) {
     return Ember.RSVP.hash({
-      tweets: Ember.$.getJSON(`/api/v1/${params.dataset}/tweets`),
-      data: Ember.$.getJSON(`/api/v1/${params.dataset}/data`),
-      dataInfo: Ember.$.getJSON(`/api/v1/${params.dataset}`),
+      tweets: Ember.$.getJSON(`${url}/api/v1/${params.dataset}/tweets`),
+      data: Ember.$.getJSON(`${url}/api/v1/${params.dataset}/data`),
+      dataInfo: Ember.$.getJSON(`${url}/api/v1/${params.dataset}`),
       dataset: params.dataset
     });
   },
--- config/environment.js
+++ config/environment.js
@@ -45,6 +45,7 @@ module.exports = function(environment) {
   }
 
   if (environment === 'production') {
+    ENV.apiUrl = 'http://usa-usa.herokuapp.com'
 
   }
 
